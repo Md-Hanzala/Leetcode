6e44ff5c9b3a8bf96783fa7b4fdfd0070ad46048
Md Hanzala <mdhanzalaengr@gmail.com>
2026-08-02 19:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (54.10%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+        for (string& op : operations) {
+            if (op == "+") {
+                int top = st.top();
+                st.pop();
+                int newTop = top + st.top();
+                st.push(top);
+                st.push(newTop);
+            } else if (op == "D") {
+                st.push(2 * st.top());
+            } else if (op == "C") {
+                st.pop();
+            } else {
+                st.push(stoi(op));
+            }
+        }
+        int sum = 0;
+        while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
+        return sum;
+    }
+};
