1369956d19f59785ace821ac0fd62eb615af6d5b
Md Hanzala <mdhanzalaengr@gmail.com>
2026-09-22 21:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (40.54%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -3,17 +3,14 @@ public:
     ListNode* reverseList(ListNode* head) {
         if (head == NULL || head->next == NULL)
             return head;
-
         ListNode* man = NULL;
         ListNode* watchman = head;
-
         while (watchman != NULL) {
-            ListNode* seeman = watchman->next;  // SAVE
-            watchman->next = man;               // REVERSE
-            man = watchman;                     // MOVE
-            watchman = seeman;                  // MOVE
+            ListNode* seeman = watchman->next;  
+            watchman->next = man;               
+            man = watchman;                    
+            watchman = seeman;                  
         }
-
         return man;
     }
 };
